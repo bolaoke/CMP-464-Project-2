e5ff92b9c706c778f3322f1b27177989fceaa58b
bolaji <bolaoke228062@gmail.com>
2023-11-06 16:41:30 -0500

    made it work

--- js/App.js
+++ js/App.js
@@ -1,56 +1,76 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-const winningCombos = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-]
-let board = ['', '', '', '', '',]
-let turn = 'x'
-let win
-function handleTurn(event) {
-    // alert("clicked")
-    let idx = squares.findIndex(function (square) {
-        return square === event.target;
-    })
-    board[idx] = turn
-    turn = turn === 'X' ? 'O' : 'X'
-    win = getWinner()
-    render()
-
-}
 function App() {
+    const winningCombos = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ]
+    const [board, setBoard] = React.useState(['', '', '', '', '', '', '', '', ''])
+    const [turn, setTurn] = React.useState('X')
+    const [win, setWin] = React.useState(null)
+
+    function getWinner() {
+        let winner = null
+        winningCombos.forEach(function (combo, index) {
+            if (
+                board[combo[0]] &&
+                board[combo[0]] === board[combo[1]] &&
+                board[combo[0]] === board[combo[2]]
+            )
+                winner = board[combo[0]]
+        })
+        return winner ? winner : board.includes('') ? null : "T"
+    }
+    function handleTurn(event) {
+        // alert("clicked")
+        // let idx = squares.findIndex(function (square) {
+        // return square === event.target;
+        //})
+        console.log(event.target)
+        let idx = event.target.id
+        let newBoard = [...board]
+        newBoard[idx] = turn
+        setBoard(newBoard)
+        let nextTurn = turn === 'X' ? '0' : 'X'
+        setTurn(nextTurn)
+        let whoWon = getWinner()
+        setWin(whoWon)
+    }
+    function Message() {
+        let message = win === 'T' ? 'That is a tie , queen!' : win ? '${win} wins the game!' : 'It is ${turn}s turn!'
+        return <h2>{message}</h2>
+    }
+
 
     return (
-
         <div>
-            <h1>Tic-React-Toe</h1>
-            <h2>let x play the game first</h2>
-
+            <h1>Tic-React-Toe!</h1>
+            <h2>It's X turn</h2>
+            <Message />
             <div className="flex-container flex-column">
-                <div className="flex-container flex-wrap" id="board"
+                <div
+                    className="flex-container flex-wrap"
+                    id="board"
                     onClick={handleTurn}>
 
-                    <div className="square"></div>
-                    <div className="square"></div>
-                    <div className="square"></div>
-                    <div className="square"></div>
-                    <div className="square"></div>
-                    <div className="square"></div>
-                    <div className="square"></div>
-                    <div className="square"></div>
-                    <div class="square"></div>
-                </div>
-            </div>
-            <button id="restartBtn" onclick="location.reload()">Restart</button>
-        </div>
+                    {board.map((data, index) => {
+                        return (
+                            <div key={index} id={index} className="square">
+                                {data}
+                            </div>
+                        )
+                    })}
 
+                </div>
+
+
+                <button id="restartBtn" onClick="location.reload()">Restart</button>
+            </div>
+        </div>
 
     )
 
--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@ console.log(squares)
 
   , [1, 4, 7], [2, 5, 8], // Columns
   [0, 4, 8], [2, 4, 6]             // Diagonals
-]; const winCombinations = [
+]; const winningCombos = [
   [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
   [0, 3, 6]
 
